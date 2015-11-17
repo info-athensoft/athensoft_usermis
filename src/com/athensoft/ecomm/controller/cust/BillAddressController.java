c7ce1f4f65ac5d5f69172a5877f5d9a127f9ccb8
Athens Zhang <athens314@hotmail.com>
2015-11-17 12:53:23 -0500

    added user_invoice_modal

--- src/com/athensoft/ecomm/controller/cust/BillAddressController.java
+++ src/com/athensoft/ecomm/controller/cust/BillAddressController.java
@@ -7,6 +7,8 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.athensoft.ecomm.entity.cust.BillAddress;
@@ -36,5 +38,23 @@ public class BillAddressController {
 		
 		model.put("billAddressList",billAddressList);
 		return mav;
+	}
+	
+	
+	@RequestMapping(value="/view_billaddress_json.do",method=RequestMethod.POST,produces="application/json")
+	@ResponseBody
+	public Map<String,Object> viewBillAddressJson(){
+		System.out.println("entering view_billaddress_json.do");
+		
+		ModelAndView mav = new ModelAndView();
+		
+		Map<String, Object> model = mav.getModel();
+		List<BillAddress> billAddressList = billAddressService.getBillAddressList();
+		
+		//test
+		System.out.println("billAddressList="+billAddressList.size());
+		
+		model.put("billAddressList",billAddressList);
+		return model;
 	}
 }
